963136cf33ad760a1a812ef2fa30349d5f4bba1b
Matheus Bittencourt Franco <matheusbittencourt@MBP-de-Matheus.lan>
2024-10-31 19:53:05 -0400

    meu primeiro let

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hello, World!");
+let name;
+let surname;
+let year_birth;
+let year_current;
+
+name = "matheus";
+surname = "franco";
+year_birth = 1984;
+year_current = 2024;
+
+console.log(name + " " + surname);
+console.log(year_current-year_birth + " anos de vida");
